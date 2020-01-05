c34bb53b2b94a699ce509dd6e36fac563c726171
Jorge Contreras <jorge.contrerasc@sansano.usm.cl>
2020-01-04 23:14:10 -0300

    parte 2

--- Actividad 2/client.py
+++ Actividad 2/client.py
@@ -1,12 +1,71 @@
-#!/usr/bin/env python
 import pika
+import threading
+import time
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+def EnviarMensaje():
+    while True:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+        global user_id
+        channel.queue_declare(queue='hello')
+        opcion = input()
+        if opcion=="1":
+            mensaje="usuarios "+str(user_id)
+            channel.basic_publish(exchange='', routing_key='hello', body=mensaje)
+        elif opcion=="2":
+            mensaje="mensajes"+str(user_id)
+            channel.basic_publish(exchange='', routing_key='hello', body=mensaje)
+        elif opcion=="3":
+            ingreso2 = input("Ingrese mensaje de la siguiente forma: (ID_destino) (MENSAJE):\n")
+            t = time.time()
+            timestamp_final = time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(t))
+            mensaje="enviar "+str(user_id)+" "+ingreso2+timestamp_final
+            channel.basic_publish(exchange='', routing_key='hello', body=mensaje)
+        else:
+            print("opcion no valida")
+        connection.close()
 
-channel.queue_declare(queue='hello')
+def RecibirMensaje():
+    global user_id
+    def callback(ch, method, properties, body):
+        mensaje=body.split(" ")
+        if mensaje[0]==user_id:
+            if mensaje[1]=="mensaje":
+                print(mensaje[4]+" ID_ORIGEN: "+mensaje[2]+ "MENSAJE: "+mensaje[3])
+            else:
+                contenido=mensaje[1:]
+                for x in contenido:
+                    print(x)
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
-connection.close()
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+    result = channel.queue_declare(queue=user_id, exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange='logs', queue=queue_name)
+
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+    user_id = input("Nuestro sistema necesita su rut para registrar un ID. Por favor ingrese su rut sin dv:\n")
+    mensaje="registrar "+user_id
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    channel.basic_publish(exchange='', routing_key='hello', body=mensaje)
+    connection.close()
+
+    tRecibir = threading.Thread(target=RecibirMensaje)
+    tEnviar = threading.Thread(target=EnviarMensaje)
+    tRecibir.start()
+    tEnviar.start()
--- Actividad 2/server.py
+++ Actividad 2/server.py
@@ -1,19 +1,83 @@
 #!/usr/bin/env python
 import pika
-
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
+import time
 
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    mensaje = str(body)
+    global usuarios
+    mensaje=mensaje[2:-1].split(" ")
+    if mensaje[0]=="registrar":
+        time.sleep(10)
+        usuarios[mensaje[1]]=[]
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+
+        channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+        result = channel.queue_declare(queue=mensaje[1], exclusive=True)
+        queue_name = result.method.queue
+
+        channel.queue_bind(exchange='logs', queue=queue_name)
+        connection.close()
+
+    if mensaje[0]=="usuarios":
+        reenviar=mensaje[1]
+        for x in usuarios.keys():
+            reenviar=reenviar+" "+x
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+
+        channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+        channel.basic_publish(exchange='logs', routing_key='', body=reenviar)
+        connection.close()
+
+    elif mensaje[0]=="mensaje":
+        reenviar=mensaje[1]
+        for x,y in usuarios.items():
+            if x==mensaje[1]:
+                for a,b in y:
+                    if a=="envio":
+                        reenviar=reenviar+" "+b
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+
+        channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+        channel.basic_publish(exchange='logs', routing_key='', body=reenviar)
+        connection.close()
+
+    else:
+        usuarios[mensaje[1]].append("envio",mensaje[4])
+        usuarios[mensaje[2]].append("recibo",mensaje[4])
+
+        log=open("log.txt","a")
+        log.write("TIMESTAMP: "+mensaje[5]+" ID_ORIGEN: "+mensaje[2]+" ID_DESTINO: "+mensaje[3]+" MENSAJE: "+mensaje[4])
+        log.close()
+        reenviar=mensaje[2]+" mensaje "+mensaje[1]+" "+mensaje[3]+" "+mensaje[4]
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+
+        channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+        channel.basic_publish(exchange='logs', routing_key='', body=mensaje)
+        connection.close()
 
 
-channel.basic_consume(
-    queue='hello', on_message_callback=callback, auto_ack=True)
+if __name__ == "__main__":
+    usuarios={}
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+    channel.queue_declare(queue='hello')
+
+    channel.basic_consume(
+        queue='hello', on_message_callback=callback, auto_ack=True)
+    print('Server running')
+    channel.start_consuming()
